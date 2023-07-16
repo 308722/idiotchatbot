9235ec284a3632ae8a242a2a13a55be74b7343fd
Lee doyul <wsax1147@naver.com>
2023-07-16 18:41:07 +0900

    Initial Upload

--- src/main/java/kr/ac/hallym/App.java
+++ src/main/java/kr/ac/hallym/App.java
@@ -1,18 +1,25 @@
 package kr.ac.hallym;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
-import net.dv8tion.jda.api.entities.Activity;
+
+import java.awt.*;
 
 public class App extends ListenerAdapter {
+    private Object GuildMessageReceivedEvent;
+
     public static void main(String[] args) {
-        JDA jda = JDABuilder.createDefault("MTEwNTA5NTA0MTQ3MjUyODQxNA.GrQmgk.VK0V2u-9XOLEk3h7FVbxaFt0hxcXg-6yKj1P9M")
+        JDA jda = JDABuilder.createDefault("MTEwNTA5NTA0MTQ3MjUyODQxNA.Gm9amP.3OCifCY2rb4A5mwAorEkv_XAl0-MPbwYBLx_eQ")
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .build();
         jda.getPresence().setActivity(Activity.playing("이세계 아이돌 시청"));
@@ -25,31 +32,78 @@ public class App extends ListenerAdapter {
             return;
 
         Message message = event.getMessage();
-        String content = message.getContentRaw();
+        String content = message.getContentRaw(); //그냥 주석 테스트
 
-        if (content.equals("!ping")) {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue();
+        switch (content) {
+            case "!ping": {
+                MessageChannel channel = event.getChannel();
+                channel.sendMessage("Pong!").queue();
+                break;
+            }
+            case "!일": {
+                MessageChannel channel = event.getChannel();
+                channel.sendMessage("싫은뎅").queue();
+                break;
+            }
+            case "!백":
+            case "!백동민": {
+                MessageChannel channel = event.getChannel();
+                channel.sendMessage("하위호환 나가라").queue();
+                break;
+            }
+            case "!이":
+            case "!이동민": {
+                MessageChannel channel = event.getChannel();
+                channel.sendMessage("이밑백").queue();
+                break;
+            }
+            case "!ㅎㅇ": {
+                MessageChannel channel = event.getChannel();
+                channel.sendMessage("안녕").queue();
+                break;
+            }
         }
-        if (content.equals("!일")) {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("싫은뎅").queue();
+        public void onGuildMessegeReceived (GuildMessageReceivedEvent event){
+
+            if (content.equals("--join")) {
+                AudioChannelUnion audioChannel = event.getMember().getVoiceState().getChannel();
+                VoiceChannel voiceChannel = audioChannel.asVoiceChannel();
+                AudioManager audioManager = event.getGuild().getAudioManager();
+                audioManager.openAudioConnection(voiceChannel);
+                MessageChannel channel = event.getChannel();
+
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle("멍청한 봇", null);
+
+                eb.setColor(Color.pink);
+                //eb.setDescription("text"); 현재 필요 없음
+
+                eb.addField("통화방에 연결됨", "정상적으로 연결됨", false);
+
+                eb.addBlankField(false);
+
+                channel.sendMessageEmbeds(eb.build()).queue();
+                while ()
+
+            } else if (content.equals("--quit")) {
+                VoiceChannel connectedChannel = (VoiceChannel) event.getGuild().getSelfMember().getVoiceState().getChannel();
+                event.getGuild().getAudioManager().closeAudioConnection();
+                MessageChannel channel = event.getChannel();
+                //channel.sendMessage("통화방 나감").queue();
+
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle("멍청한 봇", null);
+
+                eb.setColor(Color.pink);
+
+                eb.addField("통화방에서 연결 헤제", "정상적으로 해제됨", false);
+
+                eb.addBlankField(false);
+
+                channel.sendMessageEmbeds(eb.build()).queue();
+            }
+
+
         }
-        if (content.equals("!백") || content.equals("!백동민")) {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("하위호환 나가라").queue();
-        }
-        if (content.equals("!이") || content.equals("!이동민")) {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("이밑백").queue();
-        }
-        if (content.equals("!ㅎㅇ")) {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("안녕").queue();
-        }
-        if (content.equals("-join")) {
-            AudioChannelUnion voicechannel = event.getMember().getVoiceState().getChannel();
-        }
-       
     }
 }
---
+++ src/test/java/kr/ac/hallym/Handler.java
@@ -0,0 +1,8 @@
+package kr.ac.hallym;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Handler extends ListenerAdapter{
+
+}
+
